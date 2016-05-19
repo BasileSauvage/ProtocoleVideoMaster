2be629bffdde35dda2057929b6bd1dc22e6f02f6
Basile Sauvage <sauvage@unistra.fr>
2016-05-19 14:05:23 +0200

    correction de la taille du canevas en mode test

--- sources/define.h
+++ sources/define.h
@@ -36,7 +36,7 @@ enum State
 #define LAYOUT_VIDEOS_HEIGHT 720 ///< Hauteur de l'espace d'affichage des vidéos en temps normal [EDIT BASILE: phase d'entrainement]
 #define LAYOUT_VIDEOS_HEIGHT_T 900 ///< Hauteur de l'espace d'affichage des vidéos pendant la phase d'entraînement [EDIT BASILE: phase de test]
 #define LAYOUT_WIDTH 1200 ///< Largeur de l'espace de travail en temps normal
-#define LAYOUT_VIDEOS_WIDTH 960 ///< Largeur de l'espace d'affichage des vidéos en temps normal [EDIT BASILE: phase d'entrainement]
+#define LAYOUT_VIDEOS_WIDTH 1200 ///< Largeur de l'espace d'affichage des vidéos en temps normal [EDIT BASILE: phase d'entrainement]
 #define LAYOUT_VIDEOS_WIDTH_T 1200 ///< Largeur de l'espace d'affichage des vidéos pendant la phase d'entraînement [EDIT: phase de test]
 #define LAYOUT_SPACE 20 ///< espacement horizontal entre deux objets
 #define LAYOUT_LABEL 30 ///< Taille occupé par la mention référence
